9161f24eec608694609975761c4fd750de76cc35
孙志强 <1181288394@qq.com>
2020-08-30 00:29:35 +0800

    抽离订单详情公共组件

---
+++ pages/components/component/orderDetail/iconBlock/index.js
@@ -0,0 +1,25 @@
+// pages/components/component/orderDetail/iconBlock/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    iconSize: [14,20,],
+    iconColor: ['blue'],
+    iconType: ['success']
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
