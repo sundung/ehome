bbeac7fb5ba816e459560ae86612ebcdc45136a2
孙志强 <1181288394@qq.com>
2020-09-03 14:57:55 +0800

    修改waiterHeader文件到orderHeader文件夹中

--- pages/components/component/orderInService/index.js
+++ pages/components/component/orderInService/index.js
@@ -1,66 +1,53 @@
 // pages/components/component/orderWaiter/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
-  data: {
-
+  data: {},
+  // 点击获取维修清单跳转到维修清单页面
+  goToMaintainList() {
+    wx.navigateTo({
+      url:
+        '/pages/components/component/orderDetailComponents/maintainList/index',
+    })
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function () {},
 })
--- pages/order/index.js
+++ pages/order/index.js
@@ -1,105 +1,95 @@
 // pages/order/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     tabs: [
       {
-        id:0,
-        name:'待服务',
-        isActive:true
+        id: 0,
+        name: '待服务',
+        isActive: true,
       },
       {
-        id:1,
-        name:'服务中',
-        isActive:false
+        id: 1,
+        name: '服务中',
+        isActive: false,
       },
       {
-        id:2,
-        name:'已结束',
-        isActive:false
+        id: 2,
+        name: '已结束',
+        isActive: false,
       },
       {
-        id:3,
-        name:'全部',
-        isActive:false
-      }
-    ]
+        id: 3,
+        name: '全部',
+        isActive: false,
+      },
+    ],
   },
   // 自定义事件
-  handleItemChange(e){
+  handleItemChange(e) {
     // 接受组件传递过来的数据
-    const {index} = e.detail
+    const { index } = e.detail
     let tabs = JSON.parse(JSON.stringify(this.data.tabs))
-    tabs.forEach((v,i) => {
-      i === index?v.isActive=true : v.isActive=false 
+    tabs.forEach((v, i) => {
+      i === index ? (v.isActive = true) : (v.isActive = false)
       this.setData({
-        tabs
+        tabs,
       })
-     })
+    })
   },
   // 点击订单列表的待接单跳转到订单详情页面
-  goToOrderDetail(){
+  goToOrderDetail() {
     console.log('123')
     wx.navigateTo({
       url: '/pages/components/orderDetail/index',
     })
   },
+  // 点击服务中跳转到维修清单页面
+  goToMaintainList() {
+    wx.navigateTo({
+      url:
+        '/pages/components/component/orderDetailComponents/maintainList/index',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function () {},
 })
